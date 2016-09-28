5f9a12eedeecb39e1b46c78039e30fe2ec5db991
Konstantin Nazarov <mail@racktear.com>
2016-09-29 00:04:36 +0300

    Disable sandboxing
    Sandboxing can be disabled as the tarantool containers
    are now sufficiently isolated.
    
    Fixes #10

--- try/container/container.lua
+++ try/container/container.lua
@@ -9,17 +9,4 @@ box.cfg {
     wal_mode = "none"
 }
 
---
--- Add minimal sandboxing
---
-os.execute = nil
-os.exit = nil
-os.rename = nil
-os.tmpname = nil
-os.remove = nil
-io = nil
-package.loaded.socket = nil
-package.loaded.fio = nil
-package = nil
-
 require('console').listen('0.0.0.0:3313')
